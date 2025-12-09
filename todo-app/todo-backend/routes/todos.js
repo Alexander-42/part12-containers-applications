e552bcf64846b47394a356112a68e855b11b840a
Alexander-42 <alexander.oiling@gmail.com>
2025-12-09 09:50:32 +0200

    Began working on exercises for the containers module

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,13 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = req.todo;
+  res.send(todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.sendStatus(405);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
